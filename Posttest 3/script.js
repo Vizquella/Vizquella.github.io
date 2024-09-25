2c8b38cb06a7b7e962271af668c5466e4580fce2
Bagus <150510664+Vizquella@users.noreply.github.com>
2024-09-25 21:51:02 +0800

    Delete Posttest 3 directory

--- Posttest 3/script.js
+++
@@ -1,8 +0,0 @@
-function openPopup(title) {
-    document.getElementById('popupTitle').innerText = title;
-    document.getElementById('popupBox').style.display = 'flex';
-}
-
-function closePopup() {
-    document.getElementById('popupBox').style.display = 'none';
-}
